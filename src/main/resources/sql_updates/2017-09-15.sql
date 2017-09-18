a9c5a11550a004f4ec0795ca8c4b4dcfe4f85cc7
alberto <alberto@insoftar.com>
2017-09-17 21:02:21 -0500

    configuration objects

---
+++ src/main/resources/sql_updates/2017-09-15.sql
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  lacastrillov
+ * Created: 15/09/2017
+ */
+
+CREATE TABLE `marketplatform`.`configuration_object` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `type` VARCHAR(200) NOT NULL,
+  `related_entity` VARCHAR(45) NULL,
+  `related_id` INT NULL,
+  `data` TEXT NULL,
+  PRIMARY KEY (`id`));
